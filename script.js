af00281261dd3c111492cb5cc9404137db4d25e3
Muntasir <muntasirsunny05@gmail.com>
2024-04-16 12:48:38 +0600

    fetching data

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('testing');
+//Unsplash API
+const count = 10;
+const apiKey = 'Ufdfg5_BPjTUmhQu4RSFpUMZe1EbttVkD4xmKQL4udU';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch(error) {
+        //catch error here
+    }
+}
+
+// On load
+getPhotos();
